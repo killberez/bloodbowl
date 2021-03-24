735532ebb09332cd529d0b49a0debf05ee495399
Taras Liush <taras.liush@gmail.com>
2021-03-25 00:08:20 +0200

    add enducements

--- src/components/mainPaige/mainPaige.js
+++ src/components/mainPaige/mainPaige.js
@@ -1,10 +1,13 @@
 import React from "react";
+import "./mainPaige.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function MainPaige() {
   return (
-    <div>
-      <Link to="teamchoise">Create team</Link>
+    <div className="mainPaigeBody">
+      <div>
+        <Link to="teamchoise">Create team</Link>
+      </div>
     </div>
   );
 }
