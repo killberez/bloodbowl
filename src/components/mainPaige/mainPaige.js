128e55ed9243f997019158d3ccba10b11932ae02
Taras Liush <taras.liush@gmail.com>
2021-04-02 08:45:43 +0300

    add create button

--- src/components/mainPaige/mainPaige.js
+++ src/components/mainPaige/mainPaige.js
@@ -2,12 +2,15 @@ import React from "react";
 import "./mainPaige.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./mainPaige.css";
+import button from "./createTeam.jpg";
 
 function MainPaige() {
   return (
     <div className="mainDiv">
       <div>
-        <Link to="teamchoise">Create team</Link>
+        <Link to="teamchoise">
+          <img src={button} />
+        </Link>
       </div>
     </div>
   );
